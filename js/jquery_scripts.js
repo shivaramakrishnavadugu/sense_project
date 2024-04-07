e3bfa67183283eb9dea8de8e545cb690c0238477
Shivaramakrishna Vadugu <140233005+shivaramakrishnavadugu@users.noreply.github.com>
2024-04-07 11:22:16 +0530

    sense project initial upload
    create using technologies like HTML5, CSS3, JavaScript and jQuery

---
+++ js/jquery_scripts.js
@@ -0,0 +1,48 @@
+$(document).ready(function(){
+    // section four
+    $(".section_four .card_head_one").click(function(){
+        $(".section_four .accordion_one .card_body_one p").slideToggle("2000");
+    })
+    $(".section_four .card_head_two").click(function(){
+        $(".section_four .accordion_one .card_body_two p").slideToggle("2000");
+    })
+    $(".section_four .card_head_three").click(function(){
+        $(".section_four .accordion_one .card_body_three p").slideToggle("2000");
+    })
+    // section five
+    $(".section_five .card_head_one").click(function(){
+        $(".section_five .accordion_one .card_body_one p").slideToggle("2000");
+    })
+    $(".section_five .card_head_two").click(function(){
+        $(".section_five .accordion_one .card_body_two p").slideToggle("2000");
+    })
+    $(".section_five .card_head_three").click(function(){
+        $(".section_five .accordion_one .card_body_three p").slideToggle("2000");
+    })
+    // section six
+    $(".section_six .card_head_one").click(function(){
+        $(".section_six .accordion_one .card_body_one p").slideToggle("2000");
+    })
+    $(".section_six .card_head_two").click(function(){
+        $(".section_six .accordion_one .card_body_two p").slideToggle("2000");
+    })
+    $(".section_six .card_head_three").click(function(){
+        $(".section_six .accordion_one .card_body_three p").slideToggle("2000");
+    })
+    // section seven
+    $(".section_seven .card_head_one").click(function(){
+        $(".section_seven .accordion_one .card_body_one p").slideToggle("2000");
+    })
+    $(".section_seven .card_head_two").click(function(){
+        $(".section_seven .accordion_one .card_body_two p").slideToggle("2000");
+    })
+    $(".section_seven .card_head_three").click(function(){
+        $(".section_seven .accordion_one .card_body_three p").slideToggle("2000");
+    })
+    $(".buy_online").mouseenter(function(){
+        $(".select_country").css("visibility","visible");
+    })
+    $(".buy_online").mouseleave(function(){
+        $(".select_country").css("visibility","hidden");
+    })
+})
---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+nav = document.getElementById("nav");
+
+function navChange(x){
+    // x.classList.toggle("change");
+    nav.style.display = "block"
+}
+
+function closeNav() {
+    nav.style.display = "none"
+}
